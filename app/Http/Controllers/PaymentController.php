8690ae2275afeff174b171e93f9f78c4f672f57c
Ananda  Izulhaq <anandaizulhaq678@gmail.com>
2025-05-19 16:49:11 +0700

    Membuat pemesanan dengan Midtrans berdasarkan flow pemesanan

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PaymentRequest;
+use App\Models\CarDestinationPrice;
+use App\Models\Customer;
+use App\Models\MCarType;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\OwnerCar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PaymentController extends BaseController
+{
+    public function store(PaymentRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            $data = $request->validated();
+
+            $customer = new Customer();
+            $customer->name = $data['name'];
+            $customer->address = $data['address'];
+            $customer->phone_number = $data['phone_number'];
+            $customer->email = $data['email'];
+            $customer->save();
+
+            $order = new Order();
+            $order->customer_id = $customer->id;
+            $order->destination_id = $data['destination_id'];
+            $order->day = $data['day'];
+            $order->rent_date = $data['rent_date'];
+            $order->save();
+
+            $cars = $data['order_details'];
+
+            $totalPrice = 0;
+            foreach ($cars as $key => $car) {
+                $order_detail = new OrderDetail();
+                $order_detail->order_id = $order->id;
+                $order_detail->car_id = $car;
+
+                $owner_car = OwnerCar::find($car);
+                $car_type = MCarType::find($owner_car->car_type_id);
+                $car_destination_price = CarDestinationPrice::where('destination_id', $data['destination_id'])->where('car_type_id', $car_type->id)->first();
+
+                $car_destination_price = $car_destination_price->price;
+                $rentCarPrice = $car_type->rent_price;
+
+                $formula = $car_destination_price + ($order->day * $rentCarPrice);
+                $order_detail->amount = $formula;
+                $order_detail->save();
+                $totalPrice += $formula;
+            }
+            $order->total_price = $totalPrice;
+            $order->save();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->sendError($e->getMessage());
+        }
+    }
+}
---
+++ app/Http/Requests/PaymentRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'address' => 'required|string',
+            'phone_number' => 'required|string',
+            'email' => 'required|email',
+            'destination_id' => 'required|exists:m_destinations,id',
+            'day' => 'required|number',
+            'rent_date' => 'required|date',
+            'order_details' => 'required|array',
+            'order_details.*' => 'required|exists:owner_cars,id',
+        ];
+    }
+}
