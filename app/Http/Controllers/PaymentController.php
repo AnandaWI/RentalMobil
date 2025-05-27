c062cf00c1a9b0e3e355057f4c3cb8b4a0d74be0
Ananda  Izulhaq <anandaizulhaq678@gmail.com>
2025-05-27 14:27:28 +0700

    fix callback

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -103,7 +103,7 @@ class PaymentController extends BaseController
 
     public function callback(Request $request)
     {
-        $serverKey = config('app.MIDTRANS_SERVER_KEY');
+        $serverKey = config('midtrans.server_key');
         $hashedKey = hash('sha512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         if ($hashedKey == $request->signature_key) {
             if ($request->transaction_status == 'capture' || $request->transaction_status == 'settlement') {
