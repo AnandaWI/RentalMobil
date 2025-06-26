d328773e964c7b4422a5b06c235e0aaa87c2041c
Ananda  Izulhaq <anandaizulhaq678@gmail.com>
2025-06-26 13:58:07 +0700

    add uuid generator to order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Order extends Model
 {
+    protected $keyType = 'string';
+    public $incrementing = false;
+
     protected $fillable = [
         'customer_id',
         'destination_id',
@@ -16,6 +20,17 @@ class Order extends Model
         'transaction_id',
         'status'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
 
     // Relationship with Customer
     public function customer()
