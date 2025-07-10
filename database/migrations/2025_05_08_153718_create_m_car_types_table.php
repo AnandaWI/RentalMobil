05ff990a16cf88bcfba5faad6117aff4536769ec
Ananda  Izulhaq <anandaizulhaq678@gmail.com>
2025-07-10 21:13:49 +0700

    fix

--- database/migrations/2025_05_08_153718_create_m_car_types_table.php
+++ database/migrations/2025_05_08_153718_create_m_car_types_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('car_name');
             $table->integer('capacity');
             $table->decimal('rent_price', 10, 2);
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
