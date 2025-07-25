96b2f05c4161f47c16eeb52eb218e4d96573ff1c
Ananda  Izulhaq <anandaizulhaq678@gmail.com>
2025-07-25 15:48:55 +0700

    fix

--- database/migrations/2025_05_08_153949_create_history_transactions_table.php
+++ database/migrations/2025_05_08_153949_create_history_transactions_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('owner_id')->constrained('owners')->onDelete('cascade');
             $table->enum('transaction_type', ['in', 'out']);
-            $table->foreignId('order_id')->nullable()->constrained('orders')->onDelete('set null');
+            $table->uuid('order_id')->nullable();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('set null');
             $table->decimal('amount', 10, 2);
             $table->decimal('balance_now', 10, 2);
             $table->timestamps();
