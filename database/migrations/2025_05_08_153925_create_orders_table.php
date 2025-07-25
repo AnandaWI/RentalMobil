f2173626d43b8b7c74a14960138d98e0078685d8
Ananda  Izulhaq <anandaizulhaq678@gmail.com>
2025-07-25 15:45:31 +0700

    fix

--- database/migrations/2025_05_08_153925_create_orders_table.php
+++ database/migrations/2025_05_08_153925_create_orders_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
             $table->foreignId('destination_id')->constrained('m_destinations')->onDelete('cascade');
             $table->integer('day');
-            $table->decimal('total_price', 10, 2);
+            $table->decimal('total_price', 10, 2)->default(0);
             $table->date('rent_date');
-            $table->time('pick_up_time');
+            $table->time('pick_up_time')->default('00:00:00');
             $table->string('transaction_id')->nullable();
             $table->enum('status', ['pending', 'failed', 'success', 'done'])->default('pending');
             $table->timestamps();
